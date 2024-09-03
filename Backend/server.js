0b272ce57d902dbb20971dd90ece952e6200b144
CHOUAIB <128982286+chouaib-at@users.noreply.github.com>
2024-09-04 01:20:10 +0200

    creating react app

---
+++ Backend/server.js
@@ -0,0 +1,7 @@
+const express = require("express"); // Import the Express module
+const app = express(); // Create an Express application
+
+// Start the server and listen on port 8081
+app.listen(8081, () => {
+  console.log("Server is listening on port 8081");
+});
